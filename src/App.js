2da2cad3e6952d91167c3d73fadf2b7afc8a28ea
aksmr <simon.abulkalam@gmail.com>
2022-05-22 17:06:49 +0200

    new navbar

--- src/App.js
+++ src/App.js
@@ -2,37 +2,15 @@ import { Footer } from "./Components/Footer";
 import { Header } from "./Components/Header";
 import { Navbar } from "./Components/Navbar";
 import { Map } from "./Components/Map";
-import goTop from './images/icon_go-top.svg'
-
-const style = {
-	arrowTop: {
-		textDecoration: 'none',
-		color:'red',
-		position: 'fixed',
-		top:'86%',
-		left:'90%',
-		height:"10%",
-		width:"10%",
-		cursor: 'pointer',
-	},
-}
+import { Navbar2 } from "./Components/Navbar/Navbar2";
 
 export const App = () => {
 	return (
 		<>
-			<span id="top"></span>
-			<Navbar />
+			{/* <Navbar /> */}
+			<Navbar2 />
 			<Header />
-
-			<span id="acces"></span>
 			<Map/>
-
-			<img
-				src={goTop}
-				alt="Go Top"
-				onClick={() => window.location.href = '#top'}
-				style={style.arrowTop}
-			/>
 			<Footer />
 		</>
 	);
